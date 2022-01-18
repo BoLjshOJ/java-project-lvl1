a069805edaa370e315c51f5e1d00b6f1eb2cb7a5
Юшков Андрей Александрович <andrey.yushkov@gazprombank.ru>
2022-01-19 00:03:37 +0300

    task 3

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,11 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+
+    public static String readUserName() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.next();
+    }
+}
